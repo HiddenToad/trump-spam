c8513a60ecbf6e94bbd4b7b6f712a61bb73a2576
Hidden Toad <68239671+HiddenToad@users.noreply.github.com>
2021-10-22 14:58:12 -0700

    slight speed optimization

--- trumpspam.py
+++ trumpspam.py
@@ -1,15 +1,16 @@
+print("configuring selenium and launching geckodriver, please wait a moment...\n")
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import random as rand
 import string
 
-print("configuring selenium and launching geckodriver, please wait a moment...\n")
 alphabet = string.ascii_lowercase
 
 def randstr(a, b):
     return ''.join(rand.choice(alphabet) for i in range(rand.randint(a, b)))
 
 pref = webdriver.FirefoxOptions()
+
 pref.set_preference("browser.link.open_newwindow", 1)
 pref.add_argument('--headless')
 pref.add_argument('--disable-gpu')
@@ -19,12 +20,15 @@ driver = webdriver.Firefox(options=pref)
 print("done!\nrunning script...\n")
 for i in range(100_000):
     driver.get("http://truthsocial.com")
+
     first_name = randstr(4,8)
     last_name = randstr(4,8)
     email = (randstr(5,10) + rand.choice(["@gmail.com", "@icloud.com", "@me.com", "@hotmail.com", "@yahoo.com", "@hotmail.net", "@outlook.com"]))
+   
     driver.find_element(By.ID, "first_name").send_keys(first_name)
     driver.find_element(By.ID, "Last_name").send_keys(last_name)
     driver.find_element(By.ID, "email").send_keys(email)
     driver.find_element(By.ID, "offers").click()
     driver.find_element(By.CLASS_NAME, "space-y-6").submit()
-    print("falsly registered with name " + first_name + " " + last_name + " and email " + email)
+
+    print(f"falsly registered with name {first_name} {last_name} and email {email}")
