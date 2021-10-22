f9e1bd08c2cba75119a6c908d7941cbd3a21b693
Hidden Toad <68239671+HiddenToad@users.noreply.github.com>
2021-10-22 13:38:41 -0700

    switch profile to options, switch to headless

--- trumpspam.py
+++ trumpspam.py
@@ -3,8 +3,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import random as rand
 
-driver = webdriver.Firefox()
-pref = webdriver.FirefoxProfile()
+
+pref = webdriver.FirefoxOptions()
 alphabet = [char for char in "abcdefghijklmnopqrstuvwxyz"]
 def randstr(a, b):
     temp = ""
@@ -13,13 +13,16 @@ def randstr(a, b):
     return temp
 
 pref.set_preference("browser.link.open_newwindow", 1)
+pref.add_argument('--headless')
+driver = webdriver.Firefox(options=pref)
 for i in range(100_000):
     driver.get("http://truthsocial.com")
-    first_name = randstr(7,15)
-    last_name = randstr(7,15)
-    email = (randstr(5,15) + rand.choice(["@gmail.com", "@icloud.com", "@me.com", "@hotmail.com", "@yahoo.com", "@hotmail.net", "@outlook.com"]))
+    first_name = randstr(4,8)
+    last_name = randstr(4,8)
+    email = (randstr(5,10) + rand.choice(["@gmail.com", "@icloud.com", "@me.com", "@hotmail.com", "@yahoo.com", "@hotmail.net", "@outlook.com"]))
     driver.find_element(By.ID, "first_name").send_keys(first_name)
     driver.find_element(By.ID, "Last_name").send_keys(last_name)
     driver.find_element(By.ID, "email").send_keys(email)
     driver.find_element(By.ID, "offers").click()
     driver.find_element(By.CLASS_NAME, "space-y-6").submit()
+    print("falsly registered with name " + first_name + " " + last_name + " and email " + email)
